1b9dbdf8ff771f80d8b96e5e84df5f6e13513b90
vp-6599 <vaibhavpachore25@gmail.com>
2024-08-22 22:25:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Todo from './Components/Todo.js'
+
+function App() {
+  return (
+    <div className="App">
+     { <Todo/>  }
+      
+    </div>
+  );
+}
+
+export default App;
